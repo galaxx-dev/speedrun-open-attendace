b3e96b467ca9eeacb89791f1ce39ebca5ab6da37
iqbalatma <iqbalatma@gmail.com>
2022-12-24 17:51:59 +0700

    ini api

---
+++ api/app/Models/Account.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Account extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ["employee_id", "password", "is_active"];
+}
---
+++ api/database/migrations/2022_12_24_103143_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("employee_id");
+            $table->string("password", 512);
+            $table->boolean("is_active")->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
