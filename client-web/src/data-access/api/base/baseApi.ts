6c1b0b6c175bc5295f84cc556ff94ca785e67dac
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-12-27 01:54:32 +0700

    refactor(data-access): rm BASE_URL & ORIGIN

--- client-web/src/data-access/api/base/baseApi.ts
+++ client-web/src/data-access/api/base/baseApi.ts
@@ -3,9 +3,6 @@ import fetchWithCred from './fetchWithCred'
 
 export type BaseFetcher = BaseAPI['fetch']
 
-export const BASE_URL_ORIGIN = 'https://open-attendance.atma-dev.com' as const
-export const BASE_URL = `${BASE_URL_ORIGIN}/api/` as const
-
 /**
  * @version v1.1.0
  */
