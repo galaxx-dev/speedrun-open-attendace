36efa48f57441dfa740c1eb8db7e099edc81fcdc
Dio Ilham Djatiadi <apajabole1@gmail.com>
2022-12-26 13:23:19 +0700

    config(next): add swcMinify, poweredBy, & redirect
    redirects:
    - from `/` to `/dashboard`
    - from `/auth` to `/auth/login`
    
    -swcMinify to true
    - poweredByHeader to false

--- client-web/next.config.js
+++ client-web/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+      {
+        source: '/auth',
+        destination: '/auth/login',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
