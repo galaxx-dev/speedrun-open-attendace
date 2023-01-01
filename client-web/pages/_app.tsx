bb333fc4dbed43c0e771a45326ee5811e1ed308e
Dio Ilham Djatiadi <apajabole1@gmail.com>
2023-01-01 18:43:12 +0700

    refactor(_app): default site title fallback

--- client-web/pages/_app.tsx
+++ client-web/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Open Attendance</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
